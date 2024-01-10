8b98af6094676dbdfad2f3cd9a859b19bd4da48f
theparadox07 <theunknownparadox07@gmail.com>
2024-01-10 20:47:36 +0530

    first one

---
+++ pattern1.py
@@ -0,0 +1,17 @@
+#print the following pattern
+# 1 2 3 4
+# 1 2 3 4 
+# 1 2 3 4 
+# 1 2 3 4
+
+n = int(input("Enter a num"))
+i=0
+while i<n:
+    j=1
+    while j<=n:
+        print(j,end=" ")
+        j = j+1
+    print()
+    i=i+1
+
+print("Done")
