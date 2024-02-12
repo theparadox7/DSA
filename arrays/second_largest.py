134f484914fa4f49c514797ca56ea954cfe1151e
theparadox7 <149155418+theparadox7@users.noreply.github.com>
2024-02-12 17:50:39 +0000

    second largest

---
+++ arrays/second_largest.py
@@ -0,0 +1,17 @@
+arr = [0]*7
+for i in range(0,7):
+    ele = int(input("Enter"))
+    arr.append(ele)
+
+largest = arr[0]
+for i in range(1,len(arr)):
+    if arr[i] > largest:
+        largest = arr[i]
+
+#second largest
+sec_largest = -1
+for i in range(0,len(arr)):
+    if arr[i]>sec_largest and arr[i]<largest:
+        sec_largest = arr[i]
+
+print(sec_largest)
