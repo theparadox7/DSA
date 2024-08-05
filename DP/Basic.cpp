408f2f20d4d28ae081f092ab66c2e5151a9a6c79
theparadox07 <theunknownpradox07@gmail.com>
2024-08-05 11:44:35 +0530

    line change

--- DP/Basic.cpp
+++ DP/Basic.cpp
@@ -4,11 +4,8 @@ using namespace std;
 
 int f(int n ,vector<int> &dp)
 {
-    if(n <= 1)
-    return n;
-    
+    if(n <= 1) return n;
     if(dp[n] != -1) return dp[n];
-
     return dp[n] = f(n-1,dp)+f(n-2,dp);
 }
 
