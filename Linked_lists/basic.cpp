4b4c65020bdab483e3f520a85dc3b6506dec1496
theparadox07 <theunknownpradox07@gmail.com>
2024-09-01 00:21:23 +0530

    better

--- Linked_lists/basic.cpp
+++ Linked_lists/basic.cpp
@@ -22,6 +22,11 @@ int main()
     vector<int>a = {0,1,2,3,4,3};
     Node y = Node(a[0],nullptr);
     cout<<y.data<<endl;
-    cout<<y.next;
+    cout<<y.next<<endl;
+
+    // better way
+    Node* x = new Node(a[1],nullptr);
+    cout<< x->data<<endl;
+    cout<< x->next<<endl;
     return 0;
 }
