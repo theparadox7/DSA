f3969599ca0d70f8b9626e8ed1bffcf104e48623
theparadox07 <theunknownpradox07@gmail.com>
2024-09-20 22:33:51 +0530

    largest

---
+++ TCS/largest_in_array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int largestarr(vector<int>& a) {
+    int largest = a[0];
+    for (int i = 1; i < a.size(); i++)
+    {
+        if(a[i]>largest)
+        largest = a[i];
+    }
+    return largest;
+        cout<<largest;
+    
+    return a[0];
+}
+
+int main(){
+    vector<int>arr = {2,4,6,8};
+    largestarr(arr);
+
+}
+
